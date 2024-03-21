a1815b2de4fa512b4e573ee36c99984d46343089
alessio giannoccaro <mrbigalessio@gmailcom>
2024-03-21 02:02:42 +0100

    alessio del 20-03-2024 ore 02.00

---
+++ resources/views/components/center.blade.php
@@ -0,0 +1,61 @@
+<!--MESSAGGIO SCOREVOLE-->
+
+<div id="animated-text-strip">
+    <span class="marquee">IL NUOVO IPHONE 15 IN USCITA IL 16 APRILE&nbsp;</span>
+    <span class="marquee">IL NUOVO IPHONE 15 IN USCITA IL 16 APRILE&nbsp;</span>
+    <span class="marquee">IL NUOVO IPHONE 15 IN USCITA IL 16 APRILE&nbsp;</span>
+    <span class="marquee">IL NUOVO IPHONE 15 IN USCITA IL 16 APRILE&nbsp;</span>
+</div>
+
+<!--PARTI CENTRALI-->
+
+<div class="contenitore" style=
+   
+"display: flex;
+
+ height: 80vh;
+
+ width: 100%;
+
+ background-color: black;
+
+ justify-items: center; (IMPORTANTE)
+
+ margin-left: 200px;">
+
+<!--PARTE DI SINISTRA-->
+
+ <div style="flex: 1; background-color: red;">
+
+    <div class="card">
+        <div class="card-inner">
+          <div class="card-front">
+            <img src="https://m.media-amazon.com/images/I/81Wwngkh2OL._AC_UF894,1000_QL80_.jpg" alt="Ihone 15" width="500px" height="400px">
+          </div>
+          <div class="card-back">
+            <p>Dispone di un discreto display da 6.1 pollici con una risoluzione di 1179 x 2556 pixel con tecnologia Super Retina XDR OLED, HDR10, Dolby Vision, 1000 nits (HBM), 2000 nits (peak). Sul versante hardware, iPhone 15 dispone di un SOC Apple A16 Bionic (4 nm) e ha GB di ram.</p>
+          </div>
+        </div>
+      </div>
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+ <!--PARTE DI DESTRA-->
+
+ <div style="flex: 3; background-color: yellow;"></div>
+
+
+ </div>
