f4455be7255e46c432b0b0d387ee5c2093d657de
alessio giannoccaro <mrbigalessio@gmailcom>
2024-03-22 01:52:12 +0100

    alessio del 22-03-2024 ore 02.00

--- resources/views/components/center.blade.php
+++ resources/views/components/center.blade.php
@@ -16,33 +16,76 @@
  <div class="sinistra">
 
   <!--CARD GIREVOLI-->
-
-  <div class="flip-card">
-    <div class="flip-card-inner">
-        <div class="flip-card-front">
-
-          <img class="imgGirevole" src="https://m.media-amazon.com/images/I/815eN0AS-CL._AC_UF894,1000_QL80_.jpg"   alt="IPHONE 15 PRO MAX "> 
-
-          <h3>IPHONE 15 MAX PRO</h3>
-
-          <button class="btn">
-            Descrizione
-          </button>
-
-    
-            
-        </div>
-        <div class="flip-card-back">
-            <p class="title">DESCRIZIONE</p>
-            <p>Dispone di un ottimo display da 6.7 pollici con una risoluzione di 1290 x 2796 pixel con tecnologia LTPO Super Retina XDR OLED, 120Hz, HDR10, Dolby Vision, 1000 nits (typ), 2000 nits (HBM). Sul versante hardware, iPhone 15 Pro Max dispone di un SOC Apple A17 Pro (3 nm) e ha GB di ram.</p>
+  <div class="cards-wrapper">
+  <div class="card-container">
+    <div class="flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img class="imgGirevole" src="https://m.media-amazon.com/images/I/815eN0AS-CL._AC_UF894,1000_QL80_.jpg" alt="IPHONE 15 PRO MAX ">
+                <h3>IPHONE 15 MAX PRO</h3>
+                <button class="btn description-btn">Descrizione</button>
+            </div>
+            <div class="flip-card-back">
+                <p class="title">DESCRIZIONE</p>
+                <p>Dispone di un ottimo display da 6.7 pollici con una risoluzione di 1290 x 2796 pixel con tecnologia LTPO Super Retina XDR OLED, 120Hz, HDR10, Dolby Vision, 1000 nits (typ), 2000 nits (HBM). Sul versante hardware, iPhone 15 Pro Max dispone di un SOC Apple A17 Pro (3 nm) e ha GB di ram.</p>
+            </div>
         </div>
     </div>
-</div>
+
+    <div class="cards-wrapper">
+      <div class="card-container">
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img class="imgGirevole" src="https://images.samsung.com/is/image/samsung/p6pim/uk/2302/gallery/uk-galaxy-s23-s918-447009-sm-s918bzrheub-534887130?$650_519_PNG$" alt="IPHONE 15 PRO MAX ">
+                    <h3>SAMSUNG GALAXY S24</h3>
+                    <button class="btn description-btn">Descrizione</button>
+                </div>
+                <div class="flip-card-back">
+                    <p class="title">DESCRIZIONE</p>
+                    <p>Gli strumenti dotati di IA della serie Galaxy S24 - molti dei quali potenziati da Google - riassumono appunti e pagine web a danno suggerimenti di scrittura. L'IA generativa, inoltre, modifica in modo sorprendente le immagini scattate con lo smartphone</p>
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <div class="cards-wrapper">
+        <div class="card-container">
+          <div class="flip-card">
+              <div class="flip-card-inner">
+                  <div class="flip-card-front">
+                      <img class="imgGirevole" src="https://m.media-amazon.com/images/I/815eN0AS-CL._AC_UF894,1000_QL80_.jpg" alt="IPHONE 15 PRO MAX ">
+                      <h3>IPHONE 15 MAX PRO</h3>
+                      <button class="btn description-btn">Descrizione</button>
+                  </div>
+                  <div class="flip-card-back">
+                      <p class="title">DESCRIZIONE</p>
+                      <p>Dispone di un ottimo display da 6.7 pollici con una risoluzione di 1290 x 2796 pixel con tecnologia LTPO Super Retina XDR OLED, 120Hz, HDR10, Dolby Vision, 1000 nits (typ), 2000 nits (HBM). Sul versante hardware, iPhone 15 Pro Max dispone di un SOC Apple A17 Pro (3 nm) e ha GB di ram.</p>
+                  </div>
+              </div>
+          </div>
+        </div>
+
+        <div class="cards-wrapper">
+          <div class="card-container">
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                        <img class="imgGirevole" src="https://m.media-amazon.com/images/I/815eN0AS-CL._AC_UF894,1000_QL80_.jpg" alt="IPHONE 15 PRO MAX ">
+                        <h3>IPHONE 15 MAX PRO</h3>
+                        <button class="btn description-btn">Descrizione</button>
+                    </div>
+                    <div class="flip-card-back">
+                        <p class="title">DESCRIZIONE</p>
+                        <p>Dispone di un ottimo display da 6.7 pollici con una risoluzione di 1290 x 2796 pixel con tecnologia LTPO Super Retina XDR OLED, 120Hz, HDR10, Dolby Vision, 1000 nits (typ), 2000 nits (HBM). Sul versante hardware, iPhone 15 Pro Max dispone di un SOC Apple A17 Pro (3 nm) e ha GB di ram.</p>
+                    </div>
+                </div>
+            </div>
+          </div>
+  </div>
+    
 
 
-
-
- </div>
 
  <!--PARTE DESTRA-->
 
