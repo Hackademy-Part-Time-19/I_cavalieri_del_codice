aa712e75ce6937cb75d0df9720322fb3635222eb
William <william90t@hotmail.it>
2024-03-29 21:43:43 +0100

    William29

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,58 @@
-
 <x-layout>
 
-  <x-navbar />
+    <x-navbar />
 
-  <center>
-    <h1 class="titolo">LOGIN</h1>
-</center>
+    <center>
 
-  <div  class="container-sm">
+    </center>
 
 
-  <form  action="/login" method="post">
+{{-- Form Login --}}
 
-    @csrf
 
-    <div class="mb-3">
+    <div
+        style=" width:700px; height:500px;border-radius: 40px;padding: 25px 35px;border: 5px solid rgb(255, 255, 255);box-shadow: rgba(133, 189, 215, 0.8784313725) 0px 30px 30px -20px;margin: 20px;;background: #F8F9FD;background: linear-gradient(0deg, rgb(255, 255, 255) 0%, rgb(244, 247, 251) 100%);margin-left:450px;margin-top:40px">
+        <div class="heading">Login</div>
+        <form action="/login" class="form" method="POST">
+            @csrf
+            <input required="" class="input" type="email" name="email" id="email" placeholder="E-mail">
+            <input required="" class="input" type="password" name="password" id="password" placeholder="Password">
+            <span class="forgot-password"><a href="#">Hai dimenticato la password ?</a></span>
+            <input class="login-button" type="submit" value="Sign In">
 
-      <label for="email" class="form-label">INDIRIZZO EMAIL</label>
+        </form>
 
-      <input type="email" class="form-control" type="text" id="email" name="email" required>
-     
+        <div class="social-account-container">
+            <span class="title"></span>
+            <div class="social-accounts">
+                <button class="social-button google">
+                    <svg class="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 488 512">
+                        <path
+                            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z">
+                        </path>
+                    </svg></button>
+                <button class="social-button apple">
+                    <svg class="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
+                        <path
+                            d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z">
+                        </path>
+                    </svg>
+                </button>
+                <button class="social-button twitter">
+                    <svg class="svg" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                        <path
+                            d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z">
+                        </path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <span class="agreement"><a href="#">Oppure accedi con</a></span>
     </div>
 
 
-    <div class="mb-3">
-      <label for="exampleInputPassword1" class="form-label">PASSWORD</label>
 
-      <input type="password" class="form-control" type="password" id="password" name="password" required>
-
-      @error('email')<span class="text-danger">{{$message}}</span> @enderror
-    
-    </div>
-
-    <input class="formB" type="submit" value="Accedi">
-
-  </form>
-
-</div>
-
-
-
-  <!--<center>
+    <!--<center>
     <h1 class="titolo">LOGIN</h1>
 </center>
 
@@ -57,7 +69,9 @@
         <label class="formL2" for="password">Password:</label><br>
         <input class="formI" type="password" id="password" name="password" required><br>
 
-        @error('email')<span class="text-danger">{{$message}}</span> @enderror
+        @error('email')
+    <span class="text-danger">{{ $message }}</span>
+@enderror
 
         <input class="formB" type="submit" value="Accedi">
     </form>
@@ -65,103 +79,63 @@
 
 </div>-->
 
-<!--LOGO ICONE-->
-<div class="row" style="margin-top: 50px">
-    <ul class="example-2">
-     <li class="icon-content">
-       <a
-         href="https://linkedin.com/"
-         aria-label="LinkedIn"
-         data-social="linkedin"
-       >
-         <div class="filled"></div>
-         <svg
-           xmlns="http://www.w3.org/2000/svg"
-           width="16"
-           height="16"
-           fill="currentColor"
-           class="bi bi-linkedin"
-           viewBox="0 0 16 16"
-           xml:space="preserve"
-         >
-           <path
-             d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"
-             fill="currentColor"
-           ></path>
-         </svg>
-       </a>
-       <div class="tooltip">LinkedIn</div>
-     </li>
-     <li class="icon-content">
-       <a href="https://www.github.com/" aria-label="GitHub" data-social="github">
-         <div class="filled"></div>
-         <svg
-           xmlns="http://www.w3.org/2000/svg"
-           width="16"
-           height="16"
-           fill="currentColor"
-           class="bi bi-github"
-           viewBox="0 0 16 16"
-           xml:space="preserve"
-         >
-           <path
-             d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
-             fill="currentColor"
-           ></path>
-         </svg>
-       </a>
-       <div class="tooltip">GitHub</div>
-     </li>
-     <li class="icon-content">
-       <a
-         href="https://www.instagram.com/"
-         aria-label="Instagram"
-         data-social="instagram"
-       >
-         <div class="filled"></div>
-         <svg
-           xmlns="http://www.w3.org/2000/svg"
-           width="16"
-           height="16"
-           fill="currentColor"
-           class="bi bi-instagram"
-           viewBox="0 0 16 16"
-           xml:space="preserve"
-         >
-           <path
-             d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"
-             fill="currentColor"
-           ></path>
-         </svg>
-       </a>
-       <div class="tooltip">Instagram</div>
-     </li>
-     <li class="icon-content">
-       <a href="https://youtube.com/" aria-label="Youtube" data-social="youtube">
-         <div class="filled"></div>
-         <svg
-           xmlns="http://www.w3.org/2000/svg"
-           width="16"
-           height="16"
-           fill="currentColor"
-           class="bi bi-youtube"
-           viewBox="0 0 16 16"
-           xml:space="preserve"
-         >
-           <path
-             d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"
-             fill="currentColor"
-           ></path>
-         </svg>
-       </a>
-       <div class="tooltip">Youtube</div>
-     </li>
-   </ul>
-   
+    <!--LOGO ICONE-->
+    <div class="row" style="margin-top: 50px">
+        <ul class="example-2">
+            <li class="icon-content">
+                <a href="https://linkedin.com/" aria-label="LinkedIn" data-social="linkedin">
+                    <div class="filled"></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-linkedin" viewBox="0 0 16 16" xml:space="preserve">
+                        <path
+                            d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"
+                            fill="currentColor"></path>
+                    </svg>
+                </a>
+                <div class="tooltip">LinkedIn</div>
+            </li>
+            <li class="icon-content">
+                <a href="https://www.github.com/" aria-label="GitHub" data-social="github">
+                    <div class="filled"></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-github" viewBox="0 0 16 16" xml:space="preserve">
+                        <path
+                            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
+                            fill="currentColor"></path>
+                    </svg>
+                </a>
+                <div class="tooltip">GitHub</div>
+            </li>
+            <li class="icon-content">
+                <a href="https://www.instagram.com/" aria-label="Instagram" data-social="instagram">
+                    <div class="filled"></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-instagram" viewBox="0 0 16 16" xml:space="preserve">
+                        <path
+                            d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"
+                            fill="currentColor"></path>
+                    </svg>
+                </a>
+                <div class="tooltip">Instagram</div>
+            </li>
+            <li class="icon-content">
+                <a href="https://youtube.com/" aria-label="Youtube" data-social="youtube">
+                    <div class="filled"></div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-youtube" viewBox="0 0 16 16" xml:space="preserve">
+                        <path
+                            d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"
+                            fill="currentColor"></path>
+                    </svg>
+                </a>
+                <div class="tooltip">Youtube</div>
+            </li>
+        </ul>
 
 
- </div>
-</div>
+
+    </div>
+    </div>
 
 
 
