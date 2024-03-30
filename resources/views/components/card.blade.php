150aa7b73e44d26a99f3fcb419bb27500240e9d9
William <william90t@hotmail.it>
2024-03-30 15:40:28 +0100

    AggiornamentoWilliam 30-03-15:40

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,26 +1,51 @@
 <!--CARD DI ALESSIO-->
 
-<div  class="container"  >
+<div class="container">
     <div class="row">
-        
+
         <div class="col-md-4 mb-3">
-            <div class="card ml-2" style="background-color: #{{ dechex(mt_rand(0, 0xFFFFFF)) }};">
-                <img src="{{ asset('storage/logo.jpg') }}" class="card-img-top" alt="non disponibile" style="max-height: 600px;">
+            <div class="card ml-2" style="background-color: #{{ dechex(mt_rand(0, 0xffffff)) }};">
+                <img src="{{ asset('storage/logo.jpg') }}" class="card-img-top" alt="non disponibile"
+                    style="max-height: 600px;">
                 <div class="card-body" style="max-height: 300px; overflow-y: scroll;">
                     <h5 class="card-title">{{ $title }}</h5>
                     <p class="card-text">{{ $subtitle }}</p>
                     <p class="card-text" style="max-height: 300px; overflow-y: auto;">{{ $body }}</p>
                     <p class="card-text">Redatto il {{ $data }} da {{ $user }}</p>
-                    <a href="{{ $urlCategory }}" class="small text-muted d-flex justify-content-center align-items-center">{{ $category }}</a>
+                    <a href="{{ $urlCategory }}"
+                        class="small text-muted d-flex justify-content-center align-items-center">{{ $category }}</a>
                     <a href="{{ $url }}" class="btn btn-info text-white">Leggi</a>
                     <div id="contenitore_bottone_like"></div>
                 </div>
             </div>
         </div>
-        
+
     </div>
 </div>
 
+
+{{-- Card con Omar e Will --}}
+
+<div class="card">
+    <div class="card-image">
+        <img src="{{ asset('storage/logo.jpg') }}" alt="non disponibile";>
+    </div>
+    <h5 class="card-title">{{ $title }}</h5>
+    <p class="card-text">{{ $subtitle }}</p>
+    <p class="card-text" style="max-height: 300px; overflow-y: auto;">{{ $body }}</p>
+
+    <h3 class="card-body">
+        Descrizione
+    </h3>
+    <p style="color: white;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus tempore assumenda voluptas
+        error? Dolorem ipsam architecto aliquid et iusto praesentium corporis recusandae itaque totam, possimus saepe
+        iure cupiditate quibusdam sapiente!</p>
+
+    <a href="{{ $urlCategory }}"
+        class="small text-muted d-flex justify-content-center align-items-center">{{ $category }}</a>
+    <a href="{{ $url }}" class="btn btn-info text-white">Leggi</a>
+    <p class="footer">Redatto il {{ $data }} da {{ $user }}</p>
+</div>
 
 <!--CARD DI WILLIAM-->
 
@@ -31,7 +56,7 @@
     <div>
         <h5 class="card-title">{{ $title }}</h5>
         <p class="card-text">{{ $subtitle }}</p>
-        <a href="{{ $urlCategory}}" class="small text-muted d-flex justify-content-center align-items-center">{{ $category }}</a>
+        <a href="{{ $urlCategory }}" class="small text-muted d-flex justify-content-center align-items-center">{{ $category }}</a>
     </div>
     <div class="card-footer text-muted d-flex justify-content-center align-items-center">
         Redatto il {{ $data }} da {{ $user }}
--- routes/web.php
+++ routes/web.php
@@ -42,9 +42,6 @@ Route::get('/article/index' , [ArticleController::class, 'index'])->name('articl
 
 Route::get('/article/store' , [ArticleController::class, 'store'])->name('article.store');
 
-Route::get('/article/create' , [ArticleController::class, 'create'])->name('article.create');
-
-
 Route::get('/article/show/{article}' , [ArticleController::class, 'show'])->name('article.show');
 
 Route::get('/article/category/{category}' , [ArticleController::class , 'byCategory' ])->name('article.byCategory');
