87404ea85d79be162323726dbf257e20a4fe274c
alessio giannoccaro <mrbigalessio@gmailcom>
2024-04-02 19:59:18 +0200

    alessio del 02-04-2024 ore 20.00

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -33,7 +33,7 @@
                    </div>
                    <div class="mb-3">
                     <label for="image" class="form-label">Immagine:</label>
-                    <input type="fill" name="image" class="form-control" id="image">
+                    <input type="file" name="image" class="form-control" id="image">
                    </div>
                    <div class="mb-3">
                    <label for="category" class="form-label">Categoria:</label>
