feca353fd504d2e651dcff2335bb99c8ce2994a4
omarouaza <147873011+omarouaza@users.noreply.github.com>
2024-04-09 21:26:30 +0200

    omar09/04

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,10 @@
 <?php
 
-<<<<<<< HEAD
-=======
-use App\Http\Controllers\AdminController;
-
->>>>>>> 93e492db19aea523116139eee1e9b721f4ae54c5
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\RevisorController;
-use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -60,12 +55,6 @@ Route::post('/article/store' , [ArticleController::class , 'store'])->name('arti
 //ROTTE ADMIN//
 
 Route::middleware('admin')->group(function(){
-<<<<<<< HEAD
-    Route::get('/admin/dashboard' , [AdminController::class , 'dashboard'])->name('admin.dashboard');
-    Route::patch('/admin/{user}/set-admin' , [AdminController::class , 'setAdmin'])->name('admin.setAdmin');
-    Route::patch('/admin/{user}/set-revisor' , [AdminController::class , 'setRevisor'])->name('admin.setRevisor');
-    Route::patch('/admin/{user}/set-write' , [AdminController::class , 'setWriter'])->name('admin.setWriter');
-=======
 
     Route::get('/admin/dashboard' , [AdminController::class , 'dashboard'])->name('admin.dashboard');
 
@@ -80,7 +69,7 @@ Route::middleware('admin')->group(function(){
     Route::put('/admin/delete/{tag}/tag' , [AdminController::class , 'deleteSTag'])->name('admin.deleteSTag');
 
 
->>>>>>> 93e492db19aea523116139eee1e9b721f4ae54c5
+
 });
 
 Route::middleware('revisor')->group(function(){
