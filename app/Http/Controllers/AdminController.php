d5f95708e8db03a76caa23d2b5903d0b83d57c1e
alessio giannoccaro <mrbigalessio@gmailcom>
2024-04-13 02:33:53 +0200

    alessio del 13-04-2024 ore 02:33

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,32 +16,46 @@ class AdminController extends Controller
 //APPROVARE LE VARIE FUNZIONI LAVORA CON NOI //
 {
     public function dashboard(){
-        $adminRequest = User::where('is_admin', NULL)->get();
-        $revisorRequest = User::where('is_revisor', NULL)->get();
-        $writerRequest = User::where('is_writer', NULL)->get();
+        $adminRequest =User::where('is_admin' , NULL)->get();
+        $revisorRequest =User::where('is_revisor' , NULL)->get();
+        $writerRequest =User::where('is_writer' , NULL)->get();
 
-        return view('admin.dashboard', compact('adminRequest', 'revisorRequest', 'writerRequest'));
+        return view('admin.dashboard'  ,  compact('adminRequest'  ,  'revisorRequest'  ,  'writerRequest'));
     }
 
     public function setAdmin(User $user){
         $user->is_admin = true;
         $user->save();
 
-        return redirect(route('admin.dashboard'))->with('message', 'HAI CORRETTAMENTE RESO AMMINISTRATORE L\'UTENTE SCELTO');
+        return redirect(route('admin.dashboard'))->with('message' , 'HAI CORRETTAMENTE RESO AMMINISTRATORE L\'UTENTE SCELTO');
     }
 
     public function setRevisor(User $user){
         $user->is_revisor = true;
         $user->save();
 
-        return redirect(route('admin.dashboard'))->with('message', 'HAI CORRETTAMENTE RESO REVISORE L\'UTENTE SCELTO');
+        return redirect(route('admin.dashboard'))->with('message' , 'HAI CORRETTAMENTE RESO REVISORE L\'UTENTE SCELTO');
     }
 
     public function setWriter(User $user){
         $user->is_writer = true;
         $user->save();
 
-        return redirect(route('admin.dashboard'))->with('message', 'HAI CORRETTAMENTE RESO REDATTORE L\'UTENTE SCELTO');
+        return redirect(route('admin.dashboard'))->with('message' , 'HAI CORRETTAMENTE RESO REDATTORE L\'UTENTE SCELTO');
+    }
+
+    //MODIFICA TAG //
+
+    public function editTag(Request $request, Tag $tag){
+        $request->validate([
+            'name' => 'required|unique:tags',
+        ]);
+    
+        $tag->update([
+            'name' => strtolower($request->name),
+        ]);
+    
+        return redirect(route('admin.dashboard'))->with('message', 'Hai correttamente aggiornato il tag');
     }
     
     public function deleteTag(Tag $tag){
